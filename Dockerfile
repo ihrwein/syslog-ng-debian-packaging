4e35eb463a20eb535ed5dfd2e420146fbe7e79b5
root <root@localhost>
2014-12-15 14:59:42 +0000

    release: 3.7.0~alpha2-1

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,12 @@
 FROM debian:jessie
 
+ENV GIT_USER "root"
+ENV GIT_EMAIL "root@localhost"
+
 RUN apt-get -qq update && \
-	apt-get -qq install build-essential git-buildpackage libivykis-dev libmongo-client-dev libcap-dev libgeoip-dev libhiredis-dev libriemann-client-dev dh-autoreconf debhelper dh-systemd flex bison xsltproc docbook-xsl libevtlog-dev libesmtp-dev libnet1-dev libdbi-dev libssl-dev libmongo-client-dev libjson-c-dev  libwrap0-dev
+	apt-get -qq install build-essential git-buildpackage libivykis-dev libcap-dev libgeoip-dev libhiredis-dev libriemann-client-dev dh-autoreconf debhelper dh-systemd flex bison xsltproc docbook-xsl libevtlog-dev libesmtp-dev libnet1-dev libdbi-dev libssl-dev libjson-c-dev  libwrap0-dev autoconf libtool automake vim libglib2.0-dev libmongo-client-dev pkg-config
+RUN git config --global user.name $GIT_USER
+RUN git config --global user.email $GIT_EMAIL
 RUN mkdir /packaging
 VOLUME /packaging
 
--- syslog_ng.sh
+++ syslog_ng.sh
@@ -2,13 +2,16 @@
 
 generate_source_package()
 {
-    rm -Rf syslog-ng-3.6.1
-    rm -Rf dbuild
+    #rm -Rf dbuild
     rm -Rf syslog-ng*
     rm -Rf build-area
-    wget https://github.com/balabit/syslog-ng/releases/download/syslog-ng-3.6.1/syslog-ng-3.6.1.tar.gz
-    mv syslog-ng-3.6.1.tar.gz syslog-ng_3.6.1.orig.tar.gz
-    git clone -b debian/experimental https://github.com/talien/syslog-ng-debian dbuild
+    wget https://github.com/balabit/syslog-ng/releases/download/syslog-ng-3.7.0alpha2/syslog-ng-3.7.0alpha2.tar.gz
+    mv syslog-ng-3.7.0alpha2.tar.gz syslog-ng_3.7.0~alpha2.orig.tar.gz
+
+    if [ ! -d dbuild ]; then
+        git clone -b debian/experimental https://github.com/talien/syslog-ng-debian dbuild
+    fi
+
     cd dbuild
     echo "=== Building source package ==="
     git-buildpackage -S -us -uc 
@@ -19,10 +22,10 @@ generate_source_package()
 
 build_source_package()
 {
-    rm -Rf syslog-ng-3.6.1
+    rm -Rf syslog-ng-3.7.0~alpha2
     #rm -Rf tmp-debian
-    dpkg-source -x syslog-ng_3.6.1-1.dsc 
-    cd syslog-ng-3.6.1
+    dpkg-source -x syslog-ng_3.7.0~alpha2-1.dsc 
+    cd syslog-ng-3.7.0~alpha2
     echo "=== Building binary package ==="
     dpkg-buildpackage -b -us -uc 
     echo "=== Building binary package finished ==="
